98a6abea4b989e686c1d6d35842e399f7e83be28
MrWir4uk <152081630+MrWir4uk@users.noreply.github.com>
2024-07-31 15:09:50 +0300

    fff

--- main.py
+++ main.py
@@ -8,26 +8,27 @@ font.init()
 font1 = font.SysFont("Kristen ITC", 100)
 font2 = font.SysFont("Impact", 50)
 game_over_text = font1.render("GAME OVER", True, (150, 0, 0))
-restart_text = font2.render('PRESS RESTART "R" ', True, (150,0,0))
+restart_text = font2.render('PRESS R TO RESTART ', True, (150,0,0))
+start_text = font2.render('PRESS ANY KEY TO START  ', True, (150,0,0))
+
+
 
 mixer.init()
 mixer.music.load('sound.ogg')
-mixer.music.play()
+#mixer.music.play()
 mixer.music.set_volume(0.9)
 
 enemy_speed = 4 
 
-line1 = 350
-line2 = 550
-line3 = 770
-line4 = 970
 
-lines_cords = [350,550,770,970]
+
 
 screen_info = display.Info() 
 WIDTH, HEIGHT = screen_info.current_w, screen_info.current_h
+#WIDTH, HEIGHT = 1367,768
 
 window = display.set_mode((WIDTH,HEIGHT),flags=FULLSCREEN)
+#window = display.set_mode((WIDTH,HEIGHT))
 FPS = 90
 clock = time.Clock()
 
@@ -37,6 +38,12 @@ bg = transform.scale(bg,(WIDTH,HEIGHT))
 bg_y1 = 0
 bg_y2 = -HEIGHT
 
+line1 = 350 * WIDTH / 1367
+line2 = 550* WIDTH / 1367
+line3 = 770* WIDTH / 1367
+line4 = 970* WIDTH / 1367
+
+lines_cords = [line1,line2,line3,line4]
 
 player_img = image.load("car.png")
 enemy_img = image.load("car_enemy.png")
@@ -97,10 +104,13 @@ class Player(Sprite):
         self.speed_x = 10
         self.bg_speed = 5
         self.max_speed = 20
-        self.car_line = 0
-        self.rect.x = lines_cords[2]
+        self.car_line = 2
+        self.rect.x = lines_cords[self.car_line]
+        self.original = self.image
+        self.angle = 0
 
     def update(self):
+        global score_text
         key_pressed = key.get_pressed()
         old_pos = self.rect.x, self.rect.y
         if key_pressed[K_w] and self.rect.y > 200:
@@ -116,6 +126,11 @@ class Player(Sprite):
                 self.car_line -= 1
             if self.rect.x > lines_cords[self.car_line]:
                 self.rect.x -= self.speed_x
+                #if self.rect.x < lines_cords[self.car_line] + 100:
+                    # self.angle -= 1
+                    # if self.angle < 30:
+                    #     self.angle += 1
+            
             
             # self.rect.x = lines_cords[self.car_line] 
         if key_pressed[K_d] and self.rect.right < WIDTH:
@@ -123,19 +138,26 @@ class Player(Sprite):
                 self.car_line += 1
             if self.rect.x < lines_cords[self.car_line]:
                 self.rect.x += self.speed_x
+        #         if self.angle > -30:
+        #             self.angle -= 1
+        # self.image = transform.rotate(self.original, self.angle)
 
         enemy_collide = sprite.spritecollide(self, all_enemy, False, sprite.collide_mask)
         if len(enemy_collide) > 0:
             self.hp -= 100
 
-        coin_collide = sprite.spritecollide(self, all_coins, False, sprite.collide_mask)
+
+
+        coin_collide = sprite.spritecollide(self, all_coins, True, sprite.collide_mask)
         if len(coin_collide) > 0:
-            self.hp -= 100
+            self.score += 5
+            score_text = font2.render(f"Score:{self.score}", True, (255, 255, 255))
+
 
             
 
 
-player = Player(player_img, 100,150,970,600)
+player = Player(player_img, 100,150,lines_cords[1],600)
 enemy = Enemy(enemy_img,100,150)
 
 score_text = font2.render(f"Score:{player.score}", True, (255, 255, 255))
@@ -146,7 +168,7 @@ enemy_spawn_time = time.get_ticks()
 coin_spawn_time = time.get_ticks()
 spawn_interval = randint(500, 3000)
 spawn_interval_coin = randint(500, 3000)
-
+start = False
 finish = False
 run = True
 while run:
@@ -154,8 +176,8 @@ while run:
         if e.type == QUIT:
             run = False
         if e.type == KEYDOWN:
-           #if start_screen:
-                #start_screen = False
+            if not start:
+                start = True
             if e.key == K_ESCAPE:
                 run = False
                 sys.exit()
@@ -173,7 +195,7 @@ while run:
     window.blit(bg,(0,bg_y1))
     window.blit(bg,(0,bg_y2))
 
-    if finish != True:
+    if finish != True and start:
         bg_y1+=player.bg_speed
         bg_y2+=player.bg_speed
         
@@ -197,7 +219,6 @@ while run:
             timer = time.get_ticks()
         
         if now - coin_spawn_time > spawn_interval_coin:
-            all_coins = sprite.Group()
             coin = Coin(coin_img,100,100)
             coin_spawn_time = time.get_ticks()
             spawn_interval_coin = randint(500, 2500) 
@@ -208,13 +229,15 @@ while run:
 
 
         all_sprites.draw(window)
-        #window.blit(score_text,(30,30))
+        window.blit(score_text,(30,30))
         if not finish:
             all_sprites.update()
+    if not start:
+        window.blit(start_text, (WIDTH/2 - start_text.get_width()/2, HEIGHT/2 - start_text.get_height()/2 + 200))
+
     if finish:
-        window.blit(game_over_text, (300,500))
-        window.blit(restart_text, (500,600))
-    #game_over_text,WIDTH/2 - game_over_text.get_width()/2, HEIGHT/2 - game_over_text.get_height()/2
+        window.blit(restart_text, (WIDTH/2 - restart_text.get_width()/2, HEIGHT/2 - restart_text.get_height()/2 + 200))
+        window.blit(game_over_text,(WIDTH/2 - game_over_text.get_width()/2, HEIGHT/2 - game_over_text.get_height()/2))
 
     display.update()
     clock.tick(FPS)
